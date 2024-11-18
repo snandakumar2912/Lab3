0720126f6ce2c2b7906ed506678e1f2892769266
snandakumar2912 <144180105+snandakumar2912@users.noreply.github.com>
2024-11-17 22:44:08 -0600

    Task1

--- Task1/App.js
+++ Task1/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="light-content" />
       <View style={styles.content}>
-        <Text style={styles.title}>My First React Native App</Text>
+        <Text style={styles.text}>
+        My First React Native App
+        </Text>
       </View>
     </SafeAreaView>
   );
@@ -15,17 +16,17 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: 'white',
   },
   content: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
+  text: {
+    fontSize: 16, 
+    color: 'black',
+    textAlign: 'center', 
   },
 });
 
